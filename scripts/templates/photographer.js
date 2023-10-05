2b4ed9eb197bc74c65fc527dac8f30b39a6e0ddd
Svenhildr <julie.poissonnier.1989@gmail.com>
2023-10-05 16:28:22 +0200

    feature : ajout des éléments card photograph

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,39 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const article = document.createElement("article");
+        const img = document.createElement("img");
+        img.setAttribute("src", picture);
+
+        const h2 = document.createElement("h2");
         h2.textContent = name;
         article.appendChild(img);
         article.appendChild(h2);
-        return (article);
+
+        const cardInfos = document.createElement("div");
+        cardInfos.classList.add("card_infos");
+        article.appendChild(cardInfos);
+
+        let cityText = document.createElement("span");
+        cityText.classList.add("card_city");
+        cardInfos.appendChild(cityText);
+        cityText.textContent = city;
+
+        let quoteText = document.createElement("p");
+        quoteText.classList.add("card_quote");
+        cardInfos.appendChild(quoteText);
+        quoteText.textContent = tagline;
+
+        let priceText = document.createElement("span");
+        priceText.classList.add("card_price");
+        cardInfos.appendChild(priceText);
+        priceText.textContent = price;
+
+        return article;
     }
-    return { name, picture, getUserCardDOM }
+
+    return { name, picture, city, tagline, price, getUserCardDOM };
 }
